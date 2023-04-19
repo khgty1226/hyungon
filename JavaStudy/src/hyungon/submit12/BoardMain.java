01b548dd5cac5ce24622e9ebf398361fe04c4499
khgty1226 <128657360+khgty1226@users.noreply.github.com>
2023-04-19 14:16:47 +0900

    2023-04-19 테스트용

--- JavaStudy/src/hyungon/submit12/BoardMain.java
+++ JavaStudy/src/hyungon/submit12/BoardMain.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class BoardMain {
-	// ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎ
+	// ㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅎㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ
 	public static void main(String[] args) {
 		ArrayList<Board> boardList = new ArrayList<>();
 		Scanner scan = new Scanner(System.in);
